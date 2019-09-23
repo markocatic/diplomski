b4bf58c78f07f0a9293fbb03e39f4d5b755e7aa3
markocatic <marko.catic.187.15@ict.edu.rs>
2019-09-23 20:50:28 +0200

    image added to form

--- src/app/features/admin/pages/product-panel/product-panel.component.ts
+++ src/app/features/admin/pages/product-panel/product-panel.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/features/categories/services/categories.service';
+import { Product } from 'src/app/shared/models/product.model';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ProductAdminService } from '../../services/product-admin.service';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-product-panel',
@@ -6,10 +11,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-panel.component.scss']
 })
 export class ProductPanelComponent implements OnInit {
+  products: Product[];
+  oneProduct: Product;
+  productForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private categoriesService: CategoriesService,
+    private sanitizer: DomSanitizer,
+    private productAdmin: ProductAdminService
+  ) {
+    this.productForm = new FormGroup({
+      brand_id: new FormControl('', Validators.required),
+      name: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(3)]),
+      description: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(3)]),
+      description_short: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(3)]),
+      price: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(3)]),
+      new_item: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+    });
   }
 
+  ngOnInit() {
+    this.categoriesService.getAllProducts().subscribe((response: Product[]) => {
+      this.products = response;
+    });
+  }
+
+  getOneProduct(id: number) {
+    this.categoriesService.getOneProduct(id).subscribe((response: Product) => {
+      this.oneProduct = response;
+    });
+  }
+
+  deleteProduct(id: number) {
+    this.productAdmin.deleteProduct(id).subscribe((response: any) => {
+      console.log(response);
+      this.categoriesService.getAllProducts().subscribe((response: Product[]) => {
+        this.products = response;
+      });
+    });
+  }
+
+  onAdd() {
+    console.log(this.productForm.value.brand_id);
+    console.log(this.productForm.value.name);
+    console.log(this.productForm.value.description);
+    console.log(this.productForm.value.description_short);
+    console.log(this.productForm.value.price);
+    console.log(this.productForm.value.new_item);
+  }
+
+  onFileSelect(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      console.log(file);
+      this.productForm.get('image').setValue(file);
+      console.log(this.productForm.value.image);
+    }
+  }
 }
--- src/app/features/admin/services/product-admin.service.ts
+++ src/app/features/admin/services/product-admin.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductAdminService {
+  private baseUrl = 'http://localhost:8000/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  deleteProduct(id: number) {
+    return this.http.post<number>(`${this.baseUrl}/deleteProduct`, id);
+  }
 }
